508a88a887f64869183b7e305ce6e53ac99ce68a
Yannick-Neubert <yannick.neubert@gmx.de>
2022-09-23 01:42:24 +0200

    implements monad operations

--- code/part2/src/BoaInterp.hs
+++ code/part2/src/BoaInterp.hs
@@ -22,7 +22,7 @@ instance Monad Comp where
   m >>= f  = Comp (\env -> case runComp m env of 
               (Left e, out1)  -> (Left e, out1)
               (Right a, out1) -> case runComp (f a) env of
-                (Left e, out2)  -> (Left e, out1 ++ out2)
+                (Left e2, out2)  -> (Left e2, out1 ++ out2)
                 (Right x, out2) -> (Right x, out1 ++ out2))
                 
 
@@ -32,18 +32,25 @@ instance Functor Comp where
 instance Applicative Comp where
   pure = return; (<*>) = ap
 
+
+
+
 -- Operations of the monad
 abort :: RunError -> Comp a
-abort = undefined
+abort re = Comp (\_ -> (Left re, mempty))
 
 look :: VName -> Comp Value
-look = undefined
+look x = Comp (\env -> case lookup x env of
+          Nothing -> (Left (EBadVar x), mempty)
+          Just y  -> (Right y, mempty))
 
 withBinding :: VName -> Value -> Comp a -> Comp a
-withBinding = undefined
+withBinding x v m = Comp (\env -> case runComp m ((x, v):env) of
+                      (Left e, out)  -> (Left e, out)
+                      (Right a, out) -> (Right a, out))
 
 output :: String -> Comp ()
-output = undefined
+output s = Comp (\_ -> (Right (), [s]))
 
 -- Helper functions for interpreter
 truthy :: Value -> Bool
